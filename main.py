5fa4d86a08f864afdfada520c926bb9c123fb3e4
sachinsenal0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2023-08-02 02:41:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -577,9 +577,9 @@ class UserModel(db.Model):
   assistant_response = db.Column(db.String)
 
 
-def get_user_conversation_history(user_id, limit=5):
+def get_user_conversation_history(user_id, limit=10):
   with main.app_context():
-    # Query the database to retrieve the last 5 conversation history for the specified user ID
+    # Query the database to retrieve the last 10 conversation history for the specified user ID
     rows = UserModel.query.filter_by(user_id=user_id).order_by(
       UserModel.id.desc()).limit(limit).all()
 
